23807dc82d2689d322447e17dba7b3ce6dad5bce
houjing1k <houjing1k@gmail.com>
2021-03-19 02:32:34 +0800

    Add firebase streaming to other components

--- SmartLib/Front-End/src/components/seatmap/GridLines.js
+++ SmartLib/Front-End/src/components/seatmap/GridLines.js
@@ -35,6 +35,14 @@ function GridLines() {
                 <line className={"VerticalLine"} x1={1000} x2={1000} y1={0} y2={maxHeight}/>
                 <line className={"VerticalLine"} x1={1100} x2={1100} y1={0} y2={maxHeight}/>
                 <line className={"VerticalLine"} x1={1200} x2={1200} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1300} x2={1300} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1400} x2={1400} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1500} x2={1500} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1600} x2={1600} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1700} x2={1700} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1800} x2={1800} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={1900} x2={1900} y1={0} y2={maxHeight}/>
+            <line className={"VerticalLine"} x1={2000} x2={2000} y1={0} y2={maxHeight}/>
 
                 <text className="OriginText" textAnchor="start" x={0} y={25}>0</text>
 
@@ -46,6 +54,9 @@ function GridLines() {
                 <text className="HorizontalText" textAnchor="start" x={0} y={600}>600</text>
                 <text className="HorizontalText" textAnchor="start" x={0} y={700}>700</text>
                 <text className="HorizontalText" textAnchor="start" x={0} y={800}>800</text>
+            <text className="HorizontalText" textAnchor="start" x={0} y={900}>900</text>
+            <text className="HorizontalText" textAnchor="start" x={0} y={1000}>1000</text>
+            <text className="HorizontalText" textAnchor="start" x={0} y={1100}>1100</text>
 
                 <text className="VerticalText" textAnchor="end" x={100} y={25}>100</text>
                 <text className="VerticalText" textAnchor="end" x={200} y={25}>200</text>
@@ -59,6 +70,15 @@ function GridLines() {
                 <text className="VerticalText" textAnchor="end" x={1000} y={25}>1000</text>
                 <text className="VerticalText" textAnchor="end" x={1100} y={25}>1100</text>
                 <text className="VerticalText" textAnchor="end" x={1200} y={25}>1200</text>
+            <text className="VerticalText" textAnchor="end" x={1300} y={25}>1300</text>
+            <text className="VerticalText" textAnchor="end" x={1400} y={25}>1400</text>
+            <text className="VerticalText" textAnchor="end" x={1500} y={25}>1500</text>
+            <text className="VerticalText" textAnchor="end" x={1600} y={25}>1600</text>
+            <text className="VerticalText" textAnchor="end" x={1700} y={25}>1700</text>
+            <text className="VerticalText" textAnchor="end" x={1800} y={25}>1800</text>
+            <text className="VerticalText" textAnchor="end" x={1900} y={25}>1900</text>
+            <text className="VerticalText" textAnchor="end" x={2000} y={25}>2000</text>
+
         </g>
     );
 }
