b842de7f10a6bd2986f835215747d3294ac99e17
syadz <irsyadredzuan@gmail.com>
2021-04-08 16:48:52 +0800

    Code Relocation and Commentation

--- SmartLib/Front-End/src/contexts/AuthContext.js
+++ SmartLib/Front-End/src/contexts/AuthContext.js
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//      Name: AuthContext.js                                                                                            //
+//  Function: A component to aid the route function of react-router-dom to allow admin/librarian to access              //
+//            ADMIN-Only pages if they have logged in to the system. If the user is not logged in to an admin account,  //
+//            the user will not be able to access these pages such as the ModifySeatpage even if the user tries typing  //
+//            the directory in the URL.                                                                                 //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import React, {useContext, useState, useEffect} from 'react'
 import {auth} from "../firebase"
 
